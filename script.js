c6c7f644db0d5cd20e3ee780d73f082bfa894d0d
vikrm <abhishek8587singh@gmail.com>
2021-07-24 09:43:42 +0530

    add

---
+++ script.js
@@ -0,0 +1,9 @@
+const slider = new Swiper(".swiper-container", {
+  slidesPerView: 4,
+  spaceBetween: 40,
+  centeredSlides: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
