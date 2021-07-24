99c1b31f7d4b6ce807c64c326a284731f47c3124
vikrm <abhishek8587singh@gmail.com>
2021-07-24 16:31:14 +0530

    final

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const slider = new Swiper(".swiper-container", {
+const slider = new Swiper(".slide-container", {
   slidesPerView: 4,
   spaceBetween: 40,
   centeredSlides: false,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".slide-button-next",
+    prevEl: ".slide-button-prev",
+  },
+});
+
+const slider2 = new Swiper(".slider-container", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: false,
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
   },
 });
