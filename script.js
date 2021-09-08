adcb9a1ca0cb20b0d67f209b2c60f8f6a30ef47b
vikrm <abhishek8587singh@gmail.com>
2021-09-08 19:03:25 +0530

    commit

--- script.js
+++ script.js
@@ -1,20 +1,87 @@
-const slider = new Swiper(".slide-container", {
-  slidesPerView: 4,
+const swiper = new Swiper(".home-slider", {
+  slidesPerView: 1,
   spaceBetween: 40,
   centeredSlides: false,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  loop: true,
+  autoplay: {
+    duration: 1500,
+  },
+});
+
+const slider = new Swiper(".slide-container", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  centeredSlides: false,
+  speed: 600,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
   navigation: {
     nextEl: ".slide-button-next",
     prevEl: ".slide-button-prev",
   },
+  autoplay: {
+    duration: 1000,
+  },
+});
+const videoSlider = new Swiper(".swiper-video", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  centeredSlides: false,
+  speed: 600,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    duration: 1000,
+  },
+});
+const blogSlider = new Swiper(".swiper-blogs", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  centeredSlides: false,
+  speed: 600,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // autoplay: {
+  //   duration: 1000,
+  // },
 });
 
 const slider2 = new Swiper(".slider-container", {
   slidesPerView: 4,
   spaceBetween: 30,
+  speed: 600,
+  loop: true,
   centeredSlides: false,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
   navigation: {
     nextEl: ".slider-button-next",
     prevEl: ".slider-button-prev",
+  },
+  autoplay: {
+    delay: 1000,
   },
 });
 
@@ -22,5 +89,10 @@ $(document).ready(function () {
   $(".tab").click(function () {
     $(".active-tab").removeClass("active-tab");
     $(this).addClass("active-tab");
+
+    let active = $(this).index();
+
+    $(".tab-container").removeClass("active");
+    $(".tab-container").eq(active).addClass("active");
   });
 });
